646dd29efc004d2f8ac7f63b72630e1ba690883a
Ellie <102474138+ellie2718281@users.noreply.github.com>
2026-08-31 22:51:28 -0400

    Step 5: pad_id_sequence

--- model.py
+++ model.py
@@ -47,8 +47,16 @@ def decode_ids_to_tokens(ids, id_to_token):
         tokens.append(id_to_token[idx])
     return tokens
 
-# Step 5 - pad_id_sequence (not yet solved)
-# TODO: implement
+# Step 5 - pad_id_sequence
+def pad_id_sequence(ids, max_len, pad_id):
+    # return a list of length exactly max_len, padding with pad_id or truncating.
+    len_ids = len(ids)
+    if len_ids>max_len:
+        return ids[:max_len]
+    elif len_ids<max_len:
+        return ids+[pad_id]*(max_len-len_ids)
+    else:
+        return ids
 
 # Step 6 - stack_padded_sequences_to_batch (not yet solved)
 # TODO: implement
